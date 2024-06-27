250774470455d5844ea86e06855695e4fc016994
Kwanghee Choi <juice500ml@gmail.com>
2024-06-27 14:36:21 -0400

    update gm

--- main.py
+++ main.py
@@ -11,22 +11,23 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.svm import OneClassSVM
 from tqdm import tqdm
 
-from trainable_attention import train_attn
-
 
 def _get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset_pkl", type=Path, help="Dataset to evaluate")
     parser.add_argument("--n_components", type=int, default=32, help="Number of Gaussian components")
+    parser.add_argument("--n_init", type=int, default=1, help="Number of Gaussian components")
+    parser.add_argument("--n_sample", type=int, default=512, help="Number of samples to train the Gaussian mixture")
+    parser.add_argument("--covariance_type", type=str, default="full", help="Covariance type for the Gaussian mixture")
     parser.add_argument("--knn_ratio", type=float, default=0.1, help="Ratio w.r.t. samples for kNN")
     parser.add_argument("--normalize_features", action="store_true", help="Normalize SSL features")
     parser.add_argument("--skip_gop", action="store_true", help="Skip GoP calculation")
     parser.add_argument("--skip_gm", action="store_true", help="Skip GM calculation")
     parser.add_argument("--skip_knn", action="store_true", help="Skip kNN calculation")
-    parser.add_argument("--skip_attngm", action="store_true", help="Skip GM attention calculation")
     parser.add_argument("--skip_svm", action="store_true", help="Skip one-class SVM calculation")
     parser.add_argument("--skip_psvm", action="store_true", help="Skip one-class phoneme-wise SVM calculation")
     parser.add_argument("--evaluate_phonewise", action="store_true", help="Evaluate setting when each phone has a label")
+    parser.add_argument("--store_scores", type=Path, default=None, help="Path to store the resulting scores per method per phone")
     return parser.parse_args()
 
 
@@ -61,6 +62,56 @@ gops = {
 }
 
 
+def _train_gm(embs, n_components, n_init, n_sample, covariance_type, seed):
+    if len(embs) > n_sample:
+        indices = np.arange(len(embs))
+        np.random.default_rng(seed=seed).shuffle(indices)
+        embs = embs[indices[:n_sample]]
+
+    gm = GaussianMixture(
+        n_components=n_components,
+        covariance_type=covariance_type,
+        random_state=seed,
+        n_init=n_init,
+        reg_covar=1e-5,
+    )
+    gm.fit(embs)
+
+    return gm
+
+
+def _evaluate(test_df, methods, evaluate_phonewise):
+    if evaluate_phonewise:
+        dys_scores = test_df[methods + ["label"]]
+    else:
+        print("Calculating samplewise scores...")
+        dys_scores = []
+        for audio in tqdm(test_df.audio.unique()):
+            # Single sample
+            phones = test_df[test_df.audio == audio]
+
+            # # Filter out infrequent phones
+            # phones = phones[phones.phone.isin(vocab)]
+
+            # Calculate scores
+            results = {
+                method: phones[method].mean(skipna=True)
+                for method in methods
+            }
+
+            # Sum up results
+            dys_label = phones.label.unique()
+            assert len(dys_label) == 1
+            results["label"] = dys_label[0]
+            dys_scores.append(results)
+        dys_scores = pd.DataFrame(dys_scores)
+
+    return {
+        method: kendalltau(dys_scores[method], dys_scores.label, nan_policy='omit').statistic
+        for method in methods
+    }
+
+
 if __name__ == "__main__":
     args = _get_args()
     print(args)
@@ -69,7 +120,6 @@ if __name__ == "__main__":
     methods = [] if args.skip_gop else list(gops.keys())
     methods += [] if args.skip_gm else ["GM"]
     methods += [] if args.skip_knn else ["kNN"]
-    methods += [] if (args.skip_gm or args.skip_attngm) else ["GM_attn"]
     methods += [] if args.skip_svm else ["SVM"]
     methods += [] if args.skip_psvm else ["PSVM"]
 
@@ -143,31 +193,25 @@ if __name__ == "__main__":
     if not args.skip_gm:
         print("Train Gaussian mixtures...")
         gms = {}
-        for phone in tqdm(vocab):
+        for seed, phone in tqdm(enumerate(sorted(vocab))):
             embs = np.stack(train_df[train_df.phone == phone].feat.tolist())
-            gms[phone] = GaussianMixture(
+            gms[phone] = _train_gm(
+                embs,
                 n_components=args.n_components,
-                random_state=0,
-                n_init=10,
+                n_sample=args.n_sample,
+                n_init=args.n_init,
+                covariance_type=args.covariance_type,
+                seed=seed,
             )
-            gms[phone].fit(embs)
             del embs
 
         print("Calculate GM scores...")
         test_df["GM"] = np.nan
         for phone in tqdm(vocab):
             phones = test_df[test_df.phone == phone]
-            embs = phones.feat.tolist()
+            embs = np.stack(phones.feat.tolist())
             scores = gms[phone].score_samples(embs)
             test_df.loc[phones.index, "GM"] = scores
-
-        if not args.skip_attngm:
-            print("Train attention for GM..")
-            attn = train_attn(test_df, vocab_to_index)
-            test_df["GM_Attn"] = np.nan
-            for phone in vocab:
-                w = attn[vocab_to_index[phone]]
-                test_df.loc[phones.index, "GM_attn"] = w * test_df.loc[phones.index, "GM"]
 
     if not args.skip_svm:
         print("Train one-class SVM...")
@@ -194,31 +238,8 @@ if __name__ == "__main__":
             scores = svms[phone].score_samples(embs)
             test_df.loc[phones.index, "PSVM"] = scores
 
-    # Evaluate
-    if args.evaluate_phonewise:
-        dys_scores = test_df[methods + ["label"]]
-    else:
-        print("Calculating samplewise scores...")
-        dys_scores = []
-        for audio in tqdm(test_df.audio.unique()):
-            # Single sample
-            phones = test_df[test_df.audio == audio]
+    if args.store_scores:
+        test_df.drop(["feat"], axis=1).to_pickle(args.store_scores)
 
-            # Filter out infrequent phones
-            phones = phones[phones.phone.isin(vocab)]
-
-            # Calculate scores
-            results = {
-                method: phones[method].mean()
-                for method in methods
-            }
-
-            # Sum up results
-            dys_label = phones.label.unique()
-            assert len(dys_label) == 1
-            results["label"] = dys_label[0]
-            dys_scores.append(results)
-        dys_scores = pd.DataFrame(dys_scores)
-
-    for method in methods:
-        print(f"{method}: {kendalltau(dys_scores[method], dys_scores.label, nan_policy='omit').statistic:.4f}")
+    for m, kt in _evaluate(test_df, methods, args.evaluate_phonewise).items():
+        print(f"{m}: {kt:.4f}")
